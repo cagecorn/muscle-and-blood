db94d621a7cc81b91fc41bce4210b849ecea1587
cagecorn <lmcbv@naver.com>
2025-07-12 22:12:43 +0900

    Add AI and rule management skeletons

---
+++ js/managers/RuleManager.js
@@ -0,0 +1,7 @@
+// js/managers/RuleManager.js
+
+export class RuleManager {
+    constructor() {
+        console.log("\uD83D\uDCD6 RuleManager initialized. Enforcing game rules. \uD83D\uDCD6");
+    }
+}
