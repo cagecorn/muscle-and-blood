4e3c5a59e6bfd789735d0ccea7206571e2736086
cagecorn <lmcbv@naver.com>
2025-07-12 23:17:35 +0900

    Add Skeleton class and integrate RuleManager

--- data/class.js
+++ data/class.js
@@ -19,6 +19,15 @@ export const CLASSES = {
         description: '강력한 근접 공격과 방어력을 겸비한 병종.',
         skills: ['skill_melee_attack', 'skill_shield_block'],
         moveRange: 3 // 전사의 이동 거리
+    },
+    // ✨ 새롭게 추가된 해골 클래스
+    SKELETON: {
+        id: 'class_skeleton',
+        name: '해골',
+        role: CLASS_ROLES.MELEE_DPS,
+        description: '다수로 몰려오는 기본적인 언데드 적.',
+        skills: ['skill_melee_attack'],
+        moveRange: 2 // 해골의 이동 거리(예시)
     }
     // 다른 클래스들이 여기에 추가됩니다.
     // MAGE: { id: 'class_mage', ... }
